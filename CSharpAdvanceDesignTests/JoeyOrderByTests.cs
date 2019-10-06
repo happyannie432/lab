c1a82ad382043f6a0221af32b7e0ff55ed481d27
Annie <annie.tang@titansoft.com.sg>
2019-10-06 14:01:41 +0800

    extension

--- CSharpAdvanceDesignTests/JoeyOrderByTests.cs
+++ CSharpAdvanceDesignTests/JoeyOrderByTests.cs
@@ -6,41 +6,10 @@ using System.Linq;
 
 namespace CSharpAdvanceDesignTests
 {
-    [TestFixture]
-    public class JoeyOrderByTests
+    public static class MyLinqExtensions
     {
-        [Test]
-        public void orderBy_lastName()
-        {
-            var employees = new[]
-            {
-                new Employee {FirstName = "Joey", LastName = "Wang"},
-                new Employee {FirstName = "Tom", LastName = "Li"},
-                new Employee {FirstName = "Joseph", LastName = "Chen"},
-                new Employee {FirstName = "Joey", LastName = "Chen"},
-            };
-
-
-            //transform 
-            var firstPair = new KeyComparePair<string>(employee => employee.LastName, Comparer<string>.Default);
-            var secondPair = new KeyComparePair<string>(employee => employee.FirstName, Comparer<string>.Default);
-            var actual = JoeyOrderByLastNameAndFirstName(employees,
-                new ComboCompare(firstPair, secondPair));
-
-            var expected = new[]
-            {
-                new Employee {FirstName = "Joey", LastName = "Chen"},
-                new Employee {FirstName = "Joseph", LastName = "Chen"},
-                new Employee {FirstName = "Tom", LastName = "Li"},
-                new Employee {FirstName = "Joey", LastName = "Wang"},
-            };
-
-            expected.ToExpectedObject().ShouldMatch(actual);
-        }
-
-
-        private IEnumerable<Employee> JoeyOrderByLastNameAndFirstName
-            (IEnumerable<Employee> employees, ComboCompare comboCompare)
+        public static IEnumerable<Employee> AnnieOrderBy
+            (this IEnumerable<Employee> employees, ComboCompare comboCompare)
         {
             //bubble sort
             var elements = employees.ToList();
@@ -64,5 +33,45 @@ namespace CSharpAdvanceDesignTests
                 yield return minElement;
             }
         }
+    }
+
+    [TestFixture]
+    public class JoeyOrderByTests
+    {
+        [Test]
+        public void orderBy_lastName()
+        {
+            var employees = new[]
+            {
+                new Employee {FirstName = "Joey", LastName = "Wang", Age = 50},
+                new Employee {FirstName = "Tom", LastName = "Li", Age = 31},
+                new Employee {FirstName = "Joseph", LastName = "Chen", Age = 32},
+                new Employee {FirstName = "Joey", LastName = "Chen", Age = 33},
+                new Employee {FirstName = "Joey", LastName = "Wang", Age = 20},
+            };
+
+           
+            var lastNamePair = new KeyComparePair<string>(employee => employee.LastName, Comparer<string>.Default);
+            var firstNamePair = new KeyComparePair<string>(employee => employee.FirstName, Comparer<string>.Default);
+            var agePair = new KeyComparePair<int>(employee => employee.Age, Comparer<int>.Default);
+
+            var comparer = new ComboCompare(lastNamePair, firstNamePair);
+            var finalComparer = new ComboCompare(comparer, agePair);
+
+            var actual = employees.AnnieOrderBy(finalComparer);
+
+            var expected = new[]
+            {
+                new Employee {FirstName = "Joey", LastName = "Chen", Age = 33},
+                new Employee {FirstName = "Joseph", LastName = "Chen", Age = 32},
+                new Employee {FirstName = "Tom", LastName = "Li", Age = 31},
+                new Employee {FirstName = "Joey", LastName = "Wang", Age = 20},
+                new Employee {FirstName = "Joey", LastName = "Wang", Age = 50},
+            };
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        //class + change to static and change to extensions
     }
 }
