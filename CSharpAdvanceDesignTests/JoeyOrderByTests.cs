15189fd119455041566db5774f5358a13ada8ae9
Annie <annie.tang@titansoft.com.sg>
2019-10-06 11:03:31 +0800

    refactor

--- CSharpAdvanceDesignTests/JoeyOrderByTests.cs
+++ CSharpAdvanceDesignTests/JoeyOrderByTests.cs
@@ -1,12 +1,25 @@
-﻿using ExpectedObjects;
+﻿using System;
+using ExpectedObjects;
 using Lab.Entities;
 using NUnit.Framework;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CSharpAdvanceDesignTests
 {
+    public class KeyComparePair
+    {
+        public KeyComparePair(Func<Employee, string> selector, IComparer<string> comparer)
+        {
+            Selector = selector;
+            Comparer = comparer;
+        }
+
+        public Func<Employee, string> Selector { get; private set; }
+        public IComparer<string> Comparer { get; private set; }
+    }
+
     [TestFixture]
-    [Ignore("not yet")]
     public class JoeyOrderByTests
     {
         [Test]
@@ -19,13 +32,16 @@ namespace CSharpAdvanceDesignTests
                 new Employee {FirstName = "Joseph", LastName = "Chen"},
                 new Employee {FirstName = "Joey", LastName = "Chen"},
             };
-
-            var actual = JoeyOrderByLastName(employees);
+            //transform 
+            var actual = JoeyOrderByLastNameAndFirstName(
+                employees, 
+                new KeyComparePair(employee => employee.LastName, Comparer<string>.Default),
+                new KeyComparePair(employee => employee.FirstName, Comparer<string>.Default));
 
             var expected = new[]
             {
-                new Employee {FirstName = "Joseph", LastName = "Chen"},
                 new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "Joseph", LastName = "Chen"},
                 new Employee {FirstName = "Tom", LastName = "Li"},
                 new Employee {FirstName = "Joey", LastName = "Wang"},
             };
@@ -33,9 +49,43 @@ namespace CSharpAdvanceDesignTests
             expected.ToExpectedObject().ShouldMatch(actual);
         }
 
-        private IEnumerable<Employee> JoeyOrderByLastName(IEnumerable<Employee> employees)
+
+        private IEnumerable<Employee> JoeyOrderByLastNameAndFirstName
+            (IEnumerable<Employee> employees, KeyComparePair firstPair, KeyComparePair secondPair)
         {
-            throw new System.NotImplementedException();
+            //bubble sort
+            var elements = employees.ToList();
+            while (elements.Any())
+            {
+                var minElement = elements[0];
+                var index = 0;
+                for (int i = 1; i < elements.Count; i++)
+                {
+                    var employee = elements[i];
+                    if (Compare(firstPair, employee, minElement) < 0)
+                    {
+                        minElement = employee;
+                        index = i;
+                    }
+                    else if (firstPair.Comparer.Compare(firstPair.Selector(employee), firstPair.Selector(minElement)) == 0)
+                    {
+                        if (secondPair.Comparer.Compare(secondPair.Selector(employee), secondPair.Selector(minElement)) < 0)
+                        {
+                            minElement = employee;
+                            index = i;
+                        }
+                    }
+                }
+
+                elements.RemoveAt(index);
+                yield return minElement;
+            }
+        }
+
+        //extract method and make method non-static chose KeycomparePair
+        private static int Compare(KeyComparePair firstPair, Employee employee, Employee minElement)
+        {
+            return firstPair.Comparer.Compare(firstPair.Selector(employee), firstPair.Selector(minElement));
         }
     }
 }
